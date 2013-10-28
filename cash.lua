3a48f1fd4e3a3506fb0b157da59f4b8109575ed5
figgycity50 <gbrooke@outlook.com>
2013-10-28 12:23:00 +0000

    fixed #2 and made the code a LOT less lines about 18 lines less

--- cash.lua
+++ cash.lua
@@ -6,14 +6,6 @@ tArgs = {...}
 startDir = shell.dir()
 configLocation = "/.cash/cash.conf"
 
-dir = startDir
-if tArgs[0] == "--homeDir" then
-	homeDir = tArgs[1]
-else
-	homeDir = startDir
-end
-dir = homeDir
-
 --all cash vars initialized, let's read from cash.conf.
 
 --lol nothing in this version
@@ -22,62 +14,52 @@ dir = homeDir
 
 function colorText(color)
 if term.isColor() then
- term.setTextColor(color)
+term.setTextColor(color)
 end
 end
 
 local function findFile(path)
-  if fs.exists(dir..path) then
-    return true, dir..path
-  elseif fs.exists("/rom/programs/"..path) then
-    return true, "/rom/programs/"..path
-  else
-    for a,p in pairs(shell.aliases()) do --# is this even a real, and/or iterable function? o.O
-      if a == path then
-        return true, p
-      end
-    end
-  end
-  return false
+ return shell.resolveProgram(path)
 end
 
+
 function splitString(strTo)
-  local fields = {}
-  strTo:gsub("([^ ]+)", function(c) table.insert(fields, c) end)
-  return fields
+local fields = {}
+strTo:gsub("([^ ]+)", function(c) table.insert(fields, c) end)
+return fields
 end
 
 cmdHistory = {}
 
 while true do
-  if os.computerLabel() then
-	lbl = os.computerLabel()
-  else
-    lbl = os.computerID()
-  end
-  colorText(colors.yellow)
-  write("root@")
-  colorText(colors.green)
-  write(lbl)
-  colorText(colors.red)
-  write(" /"..shell.dir())
-  colorText(colors.blue)
-  write(" $ ")
-  local command = read(nil, cmdHistory)
-  table.insert(cmdHistory, command)
-  term.setTextColor(colors.white)
-  if command == "exit" then
-    break
-  else if command == "" then
-    printError("Are you crazy? Type something in!")
-  else
-    cmdTbl = splitString(command)
-    local exists, path = findFile(cmdTbl[1])
-    if exists then
-      shell.run(path, unpack(cmdTbl, 2))
-    else
-      printError("Unknown command: "..command)
-    end
-  end
+if os.computerLabel() then
+ lbl = os.computerLabel()
+else
+ lbl = os.computerID()
+end
+colorText(colors.yellow)
+write("root@")
+colorText(colors.green)
+write(lbl)
+colorText(colors.red)
+write(" /"..shell.dir())
+colorText(colors.blue)
+write(" $ ")
+local command = read(nil, cmdHistory)
+table.insert(cmdHistory, command)
+term.setTextColor(colors.white)
+if command == "exit" then
+ break
+else if command == "" then
+ printError("Are you crazy? Type something in!")
+else
+ cmdTbl = splitString(command)
+ local path = findFile(cmdTbl[1])
+ if path then
+  shell.run("/"..path, unpack(cmdTbl, 2))
+ else
+  printError("Unknown command: "..command)
+ end
+end
 end
 end
